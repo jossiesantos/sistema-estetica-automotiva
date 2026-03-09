1f210ce6178ef5e7f2a0684e9e9d4910b7b1502d
Jossie Santos <jossiesantos21@gmail.com>
2026-03-09 09:45:52 -0300

    Adicionadas telas de clientes, veículos, serviços, agendamentos e serviços pendentes

--- app.py
+++ app.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def inicio():
     return render_template("index.html")
 
+@app.route("/clientes")
+def clientes():
+    return render_template("clientes.html")
+
+@app.route("/veiculos")
+def veiculos():
+    return render_template("veiculos.html")
+
+@app.route("/servicos")
+def servicos():
+    return render_template("servicos.html")
+
+@app.route("/agendamentos")
+def agendamentos():
+    return render_template("agendamentos.html")
+
+@app.route("/servicos_pendentes")
+def servicos_pendentes():
+    return render_template("servicos_pendentes.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
